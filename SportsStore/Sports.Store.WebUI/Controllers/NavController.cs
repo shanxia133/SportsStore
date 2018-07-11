7ac86d6c218b73a300785341f3cd60515af59356
Candice <shanxia133@126.com>
2018-07-11 21:06:58 +0800

    Init the git repository

---
+++ SportsStore/Sports.Store.WebUI/Controllers/NavController.cs
@@ -0,0 +1,32 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class NavController : Controller
+    {
+        private IProductRepository repostory;
+        public NavController(IProductRepository repo)
+        {
+            repostory = repo;
+        }
+        public PartialViewResult Menu(string category=null)
+        {
+            ViewBag.SelectedCategory = category;
+            IEnumerable<string> categories = repostory.Products
+                                           .Select(x => x.Category)
+                                           .Distinct()
+                                           .OrderBy(x => x);
+            return PartialView(categories);
+        }
+        // GET: Nav
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ SportsStore/Sports.Store.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sports.Store.WebUI.MvcApplication" Language="C#" %>
